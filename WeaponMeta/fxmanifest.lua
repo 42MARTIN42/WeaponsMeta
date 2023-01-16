8541f556a0d791e25ab843d190a5031fb683625d
42MARTIN42 <98013991+42MARTIN42@users.noreply.github.com>
2023-01-16 17:34:14 +0100

    Add files via upload

---
+++ WeaponMeta/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'All weapons meta in one place'
+version '0.0.1'
+
+files {
+    'metas/**.meta'
+}
+
+data_file 'WEAPONINFO_FILE_PATCH' 'metas/**.meta'
+
+lua54 'yes'
